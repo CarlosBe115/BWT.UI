91e1c90de974a43494ff3ea7eb4da39b338f6c60
Luis-Angel-Martin-Dzul <luisprogramacion13@gmail.com>
2022-03-19 01:58:25 -0600

    add password recovery

--- BWT.UI/Controllers/UserController.cs
+++ BWT.UI/Controllers/UserController.cs
@@ -14,30 +14,67 @@ namespace BWT.UI.Controllers
 {
     public class UserController : Controller
     {
-        public static string Usertoken { get; private set; }
         HttpClientHandler _hadler = new HttpClientHandler();
         private readonly IConfiguration _configuration;
         private string apiBaseUrl;
+
         public UserController(IConfiguration configuration)
         {
             _configuration = configuration;
             apiBaseUrl = _configuration.GetValue<string>("WebApiBaseUrl");
             _hadler.ServerCertificateCustomValidationCallback = (
-                sender, cert, chain, ssLPolicyError) =>
-            { return true; };
-
+                sender, cert, chain, ssLPolicyError) => { return true; };
         }
-        
+
+        #region Vistas
+
+        public IActionResult Validation()
+        {
+            return View();
+        }
         public IActionResult Register()
         {
-
             return View();
         }
         public IActionResult RegisterInfo()
         {
-
             return View();
         }
+        public IActionResult Recover()
+        {
+            return View();
+        }
+        public IActionResult Restore(string token)
+        {
+            ViewBag.Token = token;
+            return View();
+        }
+
+        #endregion
+
+        #region Consumo
+
+        [HttpPost]
+        public async Task<IActionResult> Validation(Access access)
+        {
+            ApiResponse<Access> data;
+            using (var httpClient = new HttpClient(_hadler))
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(access), Encoding.UTF8, "application/json");
+
+                using (var response = await httpClient.PostAsync(apiBaseUrl + "access/validation", content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    data = JsonConvert.DeserializeObject<ApiResponse<Access>>(apiResponse);
+
+                }
+            }
+
+            if (data.Data != null) { FillData(data.Data); return Redirect("~/Dash/Index"); }
+            else { return Redirect("~/User/Validation"); TempData["message"] = "Usuario no valido"; }
+
+        }
+        
         [HttpPost]
         public async Task<ActionResult> Register(Access access)
         {
@@ -49,27 +86,23 @@ namespace BWT.UI.Controllers
                 StringContent content = new StringContent(JsonConvert.SerializeObject(access), Encoding.UTF8, "application/json");
                 using (var response = await httpClient.PostAsync(apiBaseUrl + "access/new", content))
                 {
-                    string apiResponse = await response.Content.ReadAsStringAsync();                 
+                    string apiResponse = await response.Content.ReadAsStringAsync();
                     request = JsonConvert.DeserializeObject<ApiResponse<bool>>(apiResponse);
 
                 }
             }
-            if (request == null)
-            {
-                TempData["message"] = "Usuario no creado"; 
-            }
-            else
-            {
-                return Redirect("~/User/RegisterInfo");
-            }
-            return View();    
+            if (request == null) TempData["message"] = "Usuario no creado";
+            else return Redirect("~/User/RegisterInfo");
+
+            return View();
         }
+        
         [HttpPost]
         public async Task<ActionResult> RegisterInfo(UserInfo userinfo)
         {
             ApiResponse<bool> request;
             userinfo.FkAccess = (int)HttpContext.Session.GetInt32("Id");
-            userinfo.ImageProfile = "https://elcomercio.pe/resizer/pfVziOV4X8Vu9nwknDc-oNItlB8=/1200x900/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/6Y2EDIISGFGVFANEVDCR5LCG34.jpg";
+            userinfo.ImageProfile = "https://cdn3.iconfinder.com/data/icons/avatars-9/145/Avatar_Cat-512.png";
             using (var httpClient = new HttpClient(_hadler))
             {
                 httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("Token"));
@@ -82,18 +115,63 @@ namespace BWT.UI.Controllers
 
                 }
             }
-            if (request == null)
-            {
-                TempData["message"] = "Información no guardada";
-            }
-            else
-            {
-                //debe mandar a la vista del acmon
-                return Redirect("https://www.youtube.com");
-            }
+            if (request == null) TempData["message"] = "Información no guardada";
+            else return Redirect("https://www.youtube.com");
+            
             return View();
         }
 
+        [HttpPost]
+        public async Task<ActionResult> Recover(Access access)
+        {
+            ApiResponse<string> data;
+            using (var httpClient = new HttpClient(_hadler))
+            {
+                using (var response = await httpClient.GetAsync(apiBaseUrl + "access/initialpr/?email=" + access.EmailAddress))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    data = JsonConvert.DeserializeObject<ApiResponse<string>>(apiResponse);
+                }
+            }
+
+            if (data.Data != null) return RedirectToAction("Restore", "User", new { @token = data.Data });
+            else return View("Recover");
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Restore(string TokenValidation, string EmailPassword1, string EmailPassword2)
+        {
+            string parameters = $"?token={TokenValidation}&password1={EmailPassword1}&password2={EmailPassword2}";
+            ApiResponse<bool> data;
+
+            using (var httpClient = new HttpClient(_hadler))
+            {
+                using (var response = await httpClient.GetAsync(apiBaseUrl + "access/finalpr/" + parameters))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    data = JsonConvert.DeserializeObject<ApiResponse<bool>>(apiResponse);
+                }
+            }
+
+            if (data.Data) return RedirectToAction("Validation", "User");
+            else return RedirectToAction("Restore", "User", new { @token = TokenValidation });
+
+        }
+
+        #endregion
+
+        #region Metodos
+
+        public void FillData(Access data)
+        {
+            HttpContext.Session.SetString("Token", data.TokenValidation);
+            HttpContext.Session.SetInt32("Id", data.Id);
+            HttpContext.Session.SetString("Email", data.EmailAddress);
+            HttpContext.Session.SetInt32("Rol", data.FkRol);
+        }
+
+        #endregion
     }
 
 }
